8f9da3c70550d6930642bd9b22f0727ad03bab03
acdprd <maksis9@ya.ru>
2019-11-29 02:06:02 +0300

    [+] strings extensions

--- utils/src/main/java/su/bnet/utils/extensions/StringExtensions.kt
+++ utils/src/main/java/su/bnet/utils/extensions/StringExtensions.kt
@@ -10,17 +10,73 @@ fun String.firstCharToUpper(): String = Utils.firstCharToUpper(this)
 
 fun String?.onlyDigits(): String? = if (this == null) null else Utils.onlyDigits(this)
 
-fun String.tryRemovePlusFromStart(): String = if (this.startsWith("+")) this@tryRemovePlusFromStart.substring(1) else this
+fun String?.isNullOrEmptyOrBlank(): Boolean = this == null || this.isEmpty() || this.isBlank()
 
-fun String.tryRemoveSevenFromStart(): String = if (this.startsWith("7")) this@tryRemoveSevenFromStart.substring(1) else this
+fun String?.isNotNullNotEmptyNotBlank(): Boolean = !this.isNullOrEmptyOrBlank()
 
-fun String.tryRemovePlusAndSevenFromStart(): String = if (this.startsWith("+7")) this@tryRemovePlusAndSevenFromStart.substring(2) else this
+fun String?.tryRemovePlusFromStart(): String? {
+    return when {
+        this == null -> null
+        this.startsWith("+") -> try {
+            this@tryRemovePlusFromStart.substring(1)
+        } catch (e: Exception) {
+            this
+        }
+        else -> this
+    }
+}
 
-fun String.tryAddPlusToStart(): String =
-    if (this.startsWith("+")) this else buildString { append("+").append(this@tryAddPlusToStart) }
+fun String?.tryRemoveSevenFromStart(): String? {
+    return when {
+        this == null -> null
+        this.startsWith("7") -> try {
+            this@tryRemoveSevenFromStart.substring(1)
+        } catch (e: Exception) {
+            this
+        }
+        else -> this
+    }
+}
 
-fun String.tryReplaceEightFromStart(): String = try {
-    if (this.startsWith("8")) buildString { append("7").append(this@tryReplaceEightFromStart.substring(1)) } else this
-} catch (e: Exception) {
-    this
+
+fun String?.tryRemovePlusAndSevenFromStart(): String? {
+    return when {
+        this == null -> null
+        this.startsWith("+7") -> try {
+            this@tryRemovePlusAndSevenFromStart.substring(2)
+        } catch (e: Exception) {
+            this
+        }
+        else -> this
+    }
+}
+
+
+fun String?.tryAddPlusToStart(): String? {
+    return when {
+        this == null -> null
+        this.startsWith("+") -> this
+        else -> buildString { append("+").append(this@tryAddPlusToStart) }
+    }
+}
+
+
+fun String?.tryReplaceEightFromStart(): String? {
+    return when {
+        this == null -> null
+        this.startsWith("8") -> try {
+            buildString { append("7").append(this@tryReplaceEightFromStart.substring(1)) }
+        } catch (e: Exception) {
+            this
+        }
+        else -> this
+    }
+}
+
+fun String.endsWith(suffix: String?): Boolean {
+    suffix?.let {
+        return endsWith(suffix, false)
+    } ?: let {
+        return false
+    }
 }
